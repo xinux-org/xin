f6206e4fd8799fc2e5675be7794a542f3d703eba
Yūri Katsuki <yuri@katsuki.moe>
2023-07-01 00:26:26 +0500

    implemented basic Nix config parser

---
+++ examples/parse.rs
@@ -0,0 +1,20 @@
+use std::path::PathBuf;
+
+use xin::NixConfig;
+
+fn main() {
+    let file = std::env::args()
+        .nth(1)
+        .expect("expected a file as an argument");
+    let path = PathBuf::from(file);
+    let settings = NixConfig::parse_file(&path);
+
+    match settings {
+        Ok(settings) => {
+            eprintln!("{settings:?}");
+        }
+        Err(settings) => {
+            eprintln!("{settings}");
+        }
+    }
+}
